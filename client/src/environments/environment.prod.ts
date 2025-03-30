9fc92755c397b1e20eb4fcf00c3d73ad7faa439c
nathan <nathan.a.zimmerman@gmail.com>
2025-03-30 15:06:06 -0400

    Update production environment URLs for API and client

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://your-production-server.com/api/sudoku'
-  };
+  production: true,
+  apiUrl: 'https://play-sudoku-api.onrender.com/api/sudoku'
+};
